6a49f6a9f1aa1ca27a9176b22d32daefd2033c1b
Richard Gajda <richardgajda11@gmail.com>
2024-01-08 23:56:49 -0500

    Sniper done

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
 
+URL = "https://fearofgod.com/collections/athletics-mens/products/heavy-fleece-hoodie"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="add")
+
+postURL = "https://discord.com/api/webhooks/1194124780765458472/5UOtknX2HUmPdZb9ft67rPQU48v3PCTmKR47F0kS1Y5ClzVaXUdos2Z_IrNy-wr1DtSy"
+
+if results.text == "Sold Out":
+    postJSON = {
+        "content": "<@326065487395946506>",
+        "embeds": [
+            {
+                "title": "Sniped items",
+                "description": "Item 1 : https://fearofgod.com/collections/athletics-mens/products/heavy-fleece-hoodie \nCurrently sold out",
+                "color": 5814783,
+            }
+        ],
+    }
+    requests.post(postURL, json=postJSON)
